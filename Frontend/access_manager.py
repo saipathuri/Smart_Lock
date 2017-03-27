26ab2dc4892828bea3fff548e766c38970eee427
Sai Pathuri <saipathuri@gmail.com>
2017-03-27 16:56:56 -0500

    fixed error handling when no update file is found

--- Frontend/access_manager.py
+++ Frontend/access_manager.py
@@ -5,7 +5,6 @@ update_location = 'update.txt'
 def remove_entry(key):
 	del users[key]
 	write_to_file()
-	print users
 
 def write_to_file():
 
@@ -59,15 +58,20 @@ def update_date(day, hour, minute):
 
 def get_date():
 	update_date_arr = None
+	file = None
 	try:
 		file = open(update_location)
 		file_contents = file.read()
 		file_by_line = file_contents.split("\n")
 		update_date_arr = [i for i in file_by_line]
 		file.close()
-		return update_date_arr
 	except:
 		update_date('1','1','00')
-		get_date()
+		file = open(update_location)
+		file_contents = file.read()
+		file_by_line = file_contents.split("\n")
+		update_date_arr = [i for i in file_by_line]
+		file.close()
+
 	return update_date_arr
 
